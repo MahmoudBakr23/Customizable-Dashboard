a5d6617ae9a1ec2670089971ea86ea74a64f3668
Mahmoud Bakr <mbakr6821@gmail.com>
2022-01-09 14:42:31 +0200

    Featured data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,44 @@
 import './home.css';
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons';
 
 const Home = () => {
     return(
         <div className="home">
-            Home Page
+            <div className="featured">
+                <div className="featuredItem">
+                <span className="featuredtitle">Revenue today</span>
+                <div className="featuredMoneyContainer">
+                    <span className="feat">$94.12</span>
+                    <span className="featuredMoneyRate">
+                        -14.5
+                        <ArrowDownward className="arrow negative" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to yesterday</span>
+            </div>
+                <div className="featuredItem">
+                <span className="featuredtitle">Revenue last 7 days</span>
+                <div className="featuredMoneyContainer">
+                    <span className="feat">$94.12</span>
+                    <span className="featuredMoneyRate">
+                        -14.5
+                        <ArrowDownward className="arrow negative" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to last week</span>
+            </div>
+                <div className="featuredItem">
+                <span className="featuredtitle">Profit last 7 days</span>
+                <div className="featuredMoneyContainer">
+                    <span className="feat">$94.12</span>
+                    <span className="featuredMoneyRate">
+                        -14.5
+                        <ArrowUpward className="arrow" />
+                    </span>
+                </div>
+                <span className="featuredSub">Compared to last week</span>
+            </div>
+            </div>
         </div>
     )
 }
